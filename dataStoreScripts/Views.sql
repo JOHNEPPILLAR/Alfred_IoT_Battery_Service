dabaa84ae7976dd31df03e38746ff5d794ab6082
JP <john.pillar@rbs.co.uk>
2019-03-31 13:36:46 +0100

    Updated view to remove inkBird

--- dataStoreScripts/Views.sql
+++ dataStoreScripts/Views.sql
@@ -1,11 +1,6 @@
 
 -- DROP VIEW vw_battery_data 
 CREATE OR REPLACE VIEW vw_battery_data AS
-SELECT last("time", "time") AS "time", last(battery, "time") AS battery, location, 'inkBird' AS device
-FROM inkbird
-WHERE time > NOW() - interval '1 hour' 
-GROUP BY location
-UNION
 SELECT last("time", "time") AS "time", last(battery, "time") AS battery, location, 'netatmo' AS device
 FROM netatmo
 WHERE time > NOW() - interval '1 hour' 
