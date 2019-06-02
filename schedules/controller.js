0f519ac30a9d1479800f30b35f1b15c89868e838
JOHNEPPILLAR <johneppillar@gmail.com>
2019-06-02 08:05:09 -0400

    Updated to node 12, use specific ver in dep

--- schedules/controller.js
+++ schedules/controller.js
@@ -10,7 +10,16 @@ const serviceHelper = require('../lib/helper.js');
 const batteryCheck = require('./battery.js');
 
 exports.collectData = async function FnCollectData() {
-  const rule = new schedule.RecurrenceRule();
+  let rule = new schedule.RecurrenceRule();
+
+  // Set first schedule
+  rule.hour = 7;
+  rule.minute = 0;
+  schedule.scheduleJob(rule, (() => { batteryCheck.getData(); })); // Set the timer
+  serviceHelper.log('info', `Battery check scheduled for ${rule.hour}:${serviceHelper.zeroFill(rule.minute, 2)}`);
+
+  // Set second schedule
+  rule = new schedule.RecurrenceRule();
   rule.hour = 18;
   rule.minute = 0;
   schedule.scheduleJob(rule, (() => { batteryCheck.getData(); })); // Set the timer
